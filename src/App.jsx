00c18899f574d8e72a4ecc96f6d9af047e4784e6
Jose Andres Ramirez Olvera <jandresrmz@yahoo.com>
2023-02-28 18:48:55 -0600

    "previo a final"

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import PhraseAuthor from './components/PhraseAuthor'
 import phrases from './data/phrases.json'
 import { useState } from 'react'
 
-
-
 function App() {
 
   const [ index, setIndex ] = useState (0)
@@ -20,6 +18,7 @@ function App() {
   return (
     <div className="App" style={{ backgroundImage: `url(./public/fondo${indexBgImg}.jpg` }}>
       <h1 className="title animate__animated animate__pulse">GALLETAS DE LA<br></br>FORTUNA</h1>
+
       <FortuneCookie
       phraseData = { phrases [index] }
       />
@@ -28,7 +27,7 @@ function App() {
       phraseData = { phrases [index] }
       />
 
-      <button className="button animate__pulse" onClick={ changePhrase }><i class='bx bx-shuffle'></i></button>
+      <button className="button animate__pulse" onClick={ changePhrase }><i class='bx bx-shuffle'> Prueba otra vez!</i></button>
 
     </div>
 
