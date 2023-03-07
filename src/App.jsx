600e52f31011de82f52582e1cb2ad631dcfd3191
Jose Andres Ramirez Olvera <jandresrmz@yahoo.com>
2023-03-06 19:12:12 -0600

    limpio

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
 
 import './App.css'
-import FortuneCookie from './components/FortuneCookie'
-import PhraseAuthor from './components/PhraseAuthor'
-import phrases from './data/phrases.json'
-import { useState } from 'react'
+
 
 function App() {
 
-  const [ index, setIndex ] = useState (0)
-
-  const changePhrase = () => {
-    setIndex(Math.floor(Math.random() * phrases.length))
-  }
-
-  const indexBgImg = Math.floor(Math.random() * 4) + 1
-
+  
   return (
     <div className="App" style={{ backgroundImage: `url(/fondo${indexBgImg}.jpg` }}>
-      <h1 className="title animate__animated animate__pulse">GALLETAS DE LA<br></br>FORTUNA</h1>
-
-      <FortuneCookie
-      phraseData = { phrases [index] }
-      />
-
-      <PhraseAuthor
-      phraseData = { phrases [index] }
-      />
-
-      <button className="button animate__pulse" onClick={ changePhrase }><i class='bx bx-shuffle'><br></br>Prueba otra vez!</i></button>
 
     </div>
 
